9a9a339d85aa11ac3693dd3bde38338ca96462d5
Hari Acharya <harryacharya2003@gmail.com>
2023-07-07 03:27:49 +0545

    ok

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Index from "./Index";
 import Header from "../components/Header";
