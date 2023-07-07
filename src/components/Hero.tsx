c7300310de2e8e0ec3e4efd5dc5bca62cfe712e6
Hari Acharya <harryacharya2003@gmail.com>
2023-07-07 21:43:54 +0545

    changed image from github to local

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,10 +4,7 @@ function Hero() {
   return (
     <div className="flex flex-col md:flex-row mt-[10vh] items-center gap-14">
       <div className="w-1/2 md:w-1/3 md:m-4">
-        <img
-          src="https://www.github.com/hariacharya80.png"
-          className="rounded-full"
-        />
+        <img src="/images/hari.jpeg" className="rounded-full" />
       </div>
       <div className="text-center md:text-left md:w-full">
         <h1 className="font-bold text-4xl">{"👋 Hello, I'm Hari Acharya"}</h1>
